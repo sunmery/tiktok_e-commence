3de83902413d6654456d94c0f86aed95df8fdebc
Mandala <xiconz@qq.com>
2024-11-24 12:02:18 +0800

    chore: change file name

--- application/checkout/internal/server/grpc.go
+++ application/checkout/internal/server/grpc.go
@@ -11,7 +11,7 @@ import (
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, greeter *service.CheckoutService, logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, checkout *service.CheckoutService, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
@@ -27,6 +27,6 @@ func NewGRPCServer(c *conf.Server, greeter *service.CheckoutService, logger log.
 		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
 	}
 	srv := grpc.NewServer(opts...)
-	v1.RegisterCheckoutServiceServer(srv, greeter)
+	v1.RegisterCheckoutServiceServer(srv, checkout)
 	return srv
 }
--- application/checkout/internal/server/http.go
+++ application/checkout/internal/server/http.go
@@ -11,7 +11,7 @@ import (
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.CheckoutService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, checkout *service.CheckoutService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
@@ -34,6 +34,6 @@ func NewHTTPServer(c *conf.Server, greeter *service.CheckoutService, logger log.
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterCheckoutServiceHTTPServer(srv, greeter)
+	v1.RegisterCheckoutServiceHTTPServer(srv, checkout)
 	return srv
 }
